480cc6b0a4500c1cd71b09e93f099899feb9bb53
MarcoPereira27 <42180338+MarcoPereira27@users.noreply.github.com>
2019-11-19 23:51:30 +0000

    service section completed

--- js/animation.js
+++ js/animation.js
@@ -1,9 +1,17 @@
 var controller = new ScrollMagic.Controller();
 var animateIn = new TimelineMax();
+var animateServices = new TimelineMax();
 
 const navbar = document.querySelector("nav");
 
 animateIn.from(".about-item", {
+  opacity: 0,
+  duration: 1,
+  y: -50,
+  stagger: 0.6
+});
+
+animateServices.from(".card", {
   opacity: 0,
   duration: 1,
   y: -50,
@@ -17,8 +25,25 @@ var scene = new ScrollMagic.Scene({
   .setTween(animateIn)
   .addTo(controller);
 
+var scene = new ScrollMagic.Scene({
+  triggerElement: "#services-container"
+})
+
+  .setTween(animateServices)
+  .addTo(controller);
+
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 0) {
     navbar.classList.add("bs-add");
   }
+
+  if (window.pageYOffset > 521) {
+    navbar.classList.add("sticky-nav");
+  } else if (window.pageYOffset < 522) {
+    navbar.classList.remove("sticky-nav");
+  }
 });
+
+// setInterval(() => {
+//   console.log(window.pageYOffset);
+// }, 500);
